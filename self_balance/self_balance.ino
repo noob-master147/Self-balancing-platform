647312fb4b9ef543de5507dbca695210aa812b30
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-05-08 17:19:57 +0530

    mark1

--- self_balance/self_balance.ino
+++ self_balance/self_balance.ino
@@ -1,9 +1,35 @@
+#include <Servo.h>
+
+Servo sx; //servo for x axis roll
+Servo sy; //servo for y axis roll
+
+float x=0;
+float y=0;
+int len=0;
+int serialdata=0;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  sx.attach(8);
+  sy.attach(9);
+  sx.write(90);
+  sy.write(90);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(Serial.available()>0)
+  int serialdata = Serial.read();
+  x = serialdata;
+  y = serialdata;
+
+  int dx = map(x,-1*len,len,0,180);
+  int dy = map(y, -1*len,len,0,180);
+
+  sx.write(dx);
+  sy.write(dy);
+  delay(5);
 
 }
