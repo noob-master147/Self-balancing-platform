0e913f1aeaf9ba3e7dd20f8e73fbbf7b5b1891f5
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-05-17 23:10:00 +0530

    added integral windup

--- PID_Control/PID_Control.ino
+++ PID_Control/PID_Control.ino
@@ -1,9 +1,39 @@
+double sensed_output, setpoint, control_signal;
+double kp, ki, kd;
+int T =50;
+unsigned long last_time;
+double total_error, last_error;
+
 void setup() {
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  PID_Control();
 
+}
+
+void PID_Control()
+{
+
+  unsigned long current_time =millis();
+  int delta_time = current_time - last_time;
+  
+  if(delta_time>T)
+  {
+
+    double error = setpoint - sensed_output;
+    total_error +=error;
+    if(total_error>=255) total_error = 255;
+    else if(total_error<=0) total_error = 0;
+    double delta_error = error -last_error;
+
+    control_signal = kp*error +ki*T*total_error +(kd/T)*delta_error;
+    if(total_error>=255) total_error = 255;
+    else if(total_error<=0) total_error = 0;
+    
+    last_error = error;
+    last_time = current_time;
+  }
 }
